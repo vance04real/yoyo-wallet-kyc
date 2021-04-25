c4c924a5e9ab12d6d0d5b40386f3f99e05c94fec
Evans K F Chikuni <vance04real@gmail.com>
2021-04-25 21:31:43 +0200

    fixes #1
    created Customer entity class with Id set to Auto generation strategy

---
+++ src/main/java/za/co/yoyowallet/kyc/domain/Customer.java
@@ -0,0 +1,65 @@
+package za.co.yoyowallet.kyc.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created  25/04/2021 - 21:29
+ * Project  kyc
+ * Author   Evans K F C
+ **/
+
+@Entity
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+    private Long age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
